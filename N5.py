9eabbed291040057e9a78a7b6f5aeaa70581f82e
mark <frizenmarkv@gmail.com>
2025-10-30 21:17:03 +0300

    Задание 2

--- N5.py
+++ N5.py
@@ -7,5 +7,6 @@ def print_fibonacci(n):
     fib_gen = fibonacci()
     for _ in range(n):
         print(next(fib_gen))
+
 if __name__ == "__main__":
-    print_fibonacci(10)
+    print_fibonacci(20)
