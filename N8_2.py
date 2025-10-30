9a9607dd4a6cbc0ab7f904f2f2c382e30a14e876
mark <frizenmarkv@gmail.com>
2025-10-30 21:18:03 +0300

    Задание 5

--- N8_2.py
+++ N8_2.py
@@ -1,28 +1,34 @@
 import csv
 from io import StringIO
+
 csv_data = """name,age
 Alice,25
-Bob,35
+Bob,15
 Charlie,40
-Diana,28
+Diana,8
 Eve,31
 """
+
 def read_csv(data):
     try:
         return list(csv.DictReader(StringIO(data)))
     except Exception as e:
         print(f"Ошибка чтения CSV: {e}")
         return []
+
 def is_adult(user):
-    return int(user.get('age', 0)) > 30
+    return int(user['age']) > 18
+
 def to_uppercase(user):
-    user['name'] = user.get('name', '').upper()
+    user['name'] = user['name'].upper()
     return user
+
 def process_users(data):
     users = read_csv(data)
     filtered_users = list(filter(is_adult, users))
     transformed_users = list(map(to_uppercase, filtered_users))
     return transformed_users
+
 if __name__ == "__main__":
     result = process_users(csv_data)
     for user in result:
