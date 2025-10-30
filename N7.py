51c55062e5bd4a228659c4a129c669735baab130
mark <frizenmarkv@gmail.com>
2025-10-30 21:17:38 +0300

    Задание 3

--- N7.py
+++ N7.py
@@ -5,12 +5,23 @@ def log_decorator(func):
         _log_result(func, result)
         return result
     return wrapper
+
+
 def _log_call(func, args, kwargs):
     print(f"Вызов функции '{func.__name__}' с аргументами: args={args}, kwargs={kwargs}")
 def _log_result(func, result):
     print(f"Функция '{func.__name__}' вернула: {result}")
+
+
+
 @log_decorator
 def add(a, b):
     return a + b
+
+@log_decorator
+def minus(a, b):
+    return a - b
+
 if __name__ == "__main__":
     add(3, 5)
+    minus(20, 4)
